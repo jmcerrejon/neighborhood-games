51ee6436b014c39a9416adef2dbf423102cf356f
Jose Manuel Cerrejon Gonzalez <ulysess@gmail.com>
2020-03-19 19:40:35 +0100

    Improve index.html. Add player.css, player.js

---
+++ public/js/player.js
@@ -0,0 +1,30 @@
+const socket = io()
+$(() => {
+	socket.on('hello', () => {
+		$('#chat')
+			.empty()
+			.append('<h3>Bienvenido al chat</h3>')
+	})
+})
+
+$('form').on('submit', event => {
+	event.preventDefault()
+	let msg = {
+		name: $('#username').val().trim(),
+		content: $('#content').val().trim()
+	}
+	if ((msg.name.length > 0) && (msg.content.length > 0)) {
+		socket.emit('new_msg', msg)
+		$('#username').attr('disabled', 'disabled')
+		$('#content').val('').focus()
+	}
+})
+
+socket.on('spread_msg', msg => {
+	$('#chat')
+		.append(`<b>${special(msg.name)}</b> ${special(msg.content)}<br>`)
+})
+
+const special = str => {
+	return (str.replace(/</gi, '&lt;')).replace(/>/gi, '&gt;')
+}
