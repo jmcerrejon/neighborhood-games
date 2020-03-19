103dbaff9819abb6dd18d37bbb0df4597105fea9
Jose Manuel Cerrejon Gonzalez <ulysess@gmail.com>
2020-03-19 19:40:58 +0100

    Add socket disconnect on app.js

--- app.js
+++ app.js
@@ -5,10 +5,14 @@ const io = require('socket.io')(http)
 const players = []
 
 io.on('connection', socket => {
-	console.log('New user connected!')
+	console.log('New user connected | Id: ', socket.id)
 	socket.emit('hello')
 	socket.on('new_msg', msg => {
 		io.sockets.emit('spread_msg', msg)
+
+	})
+	socket.on('disconnect', soket => {
+		console.log('User disconnected | Id: ', socket.id)
 	})
 })
 
