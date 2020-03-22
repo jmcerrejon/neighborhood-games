eef32acaea9f0e17c48b78023341c86c2025de18
Jose Manuel Cerrejon Gonzalez <ulysess@gmail.com>
2020-03-22 08:47:39 +0100

    Move setMessage to helper.js.

--- public/js/helper.js
+++ public/js/helper.js
@@ -1,17 +1,24 @@
 const getUrlParam = (parameter, defaultvalue = 'default') => {
 	return (window.location.href.indexOf(parameter) > -1) ?
-		getUrlVars()[parameter] : defaultvalue;
+		getUrlVars()[parameter] : defaultvalue
 }
 
 const getUrlVars = () => {
-    let vars = [];
+    let vars = []
     window.location.href.replace(/[?&]+([^=&]+)=([^&]*)/gi, function(m,key,value) {
-        vars[key] = decodeURIComponent(value).replace(/\+/g, ' ');
-    });
+        vars[key] = decodeURIComponent(value).replace(/\+/g, ' ')
+    })
 
-    return vars;
+    return vars
 }
 
 const setButtonColor = (divId, color) => {
 	document.getElementById(divId).className = color
+}
+
+const setMessage = msg => {
+	$('#chat')
+		.append(`${msg}</br>`)
+	const divChat = $('#chat')
+	divChat.scrollTop(divChat.prop("scrollHeight"))
 }
