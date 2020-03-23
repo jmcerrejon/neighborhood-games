d6a16b2452fd8e200c97362ff8eec1a14900b8db
Jose Manuel Cerrejon Gonzalez <ulysess@gmail.com>
2020-03-23 12:39:20 +0100

    Refactor code.

--- app.js
+++ app.js
@@ -2,43 +2,32 @@ const express = require('express')
 const app = express()
 const http = require('http').Server(app)
 const io = require('socket.io')(http)
-const players = require('./src/player')()
+const socketReq = require('./src/socket-core')
 
 io.on('connection', socket => {
-	socket.on('new_msg', msg => {
-		io.sockets.emit('spread_msg', msg)
-	})
+	const {
+		newMsg,
+		getPlayers,
+		newPlayer,
+		disconnect,
+		buttonReady,
+		buttonPressed,
+		checkAnswer
+	} = socketReq(socket, io)
 
-	socket.on('get_players', () => {
-		socket.emit('get_players', players.getAvailablePlayers())
-	})
+	socket.on('new_msg', newMsg)
 
-	socket.on('new_player', player => {
-		player.id = socket.id
-		players.addPlayer(player)
-		console.log('New player:', JSON.stringify(player, null, 2))
+	socket.on('get_players', getPlayers)
 
-		io.sockets.emit('get_players', players.getAvailablePlayers())
-	})
+	socket.on('new_player', newPlayer)
 
-	socket.on('disconnect', soket => {
-		players.delPlayer(socket.id)
-		io.sockets.emit('get_players', players.getAvailablePlayers())
-	})
+	socket.on('disconnect', disconnect)
+	
+	socket.on('button_ready', buttonReady)
 
-	socket.on('button_ready', isReady => {
-		io.sockets.emit('change_button', isReady)
-	})
+	socket.on('button_pressed', buttonPressed)
 
-	socket.on('button_pressed', player => {
-		console.log('Player:', player)
-		io.sockets.emit('change_button', false)
-		io.sockets.emit('team_clicked', player)
-	})
-
-	socket.on('check_answer', ({isValid, playerId}) => {
-		io.sockets.emit('change_button', !isValid)
-	})
+	socket.on('check_answer', checkAnswer)
 })
 
 app.use(express.static(__dirname + '/public'))
---
+++ src/socket-core.js
@@ -0,0 +1,46 @@
+const players = require('./player')()
+
+module.exports = (socket, io) => {
+	const newMsg = msg => {
+		io.sockets.emit('spread_msg', msg)
+	}
+
+	const getPlayers = () => {
+		io.sockets.emit('get_players', players.getAvailablePlayers())
+	}
+
+	const newPlayer = player => {
+		player.id = socket.id
+		players.addPlayer(player)
+		console.log('New player:', JSON.stringify(player, null, 2))
+		io.sockets.emit('get_players', players.getAvailablePlayers())
+	}
+
+	const disconnect = () => {
+		players.delPlayer(socket.id)
+		io.sockets.emit('get_players', players.getAvailablePlayers())
+	}
+
+	const buttonReady = isReady => {
+		io.sockets.emit('change_button', isReady)
+	}
+
+	const buttonPressed = player => {
+		io.sockets.emit('change_button', false)
+		io.sockets.emit('team_clicked', player)
+	}
+
+	const checkAnswer = ({isValid}) => {
+		io.sockets.emit('change_button', !isValid)
+	}
+
+	return {
+		newMsg,
+		getPlayers,
+		newPlayer,
+		disconnect,
+		buttonReady,
+		buttonPressed,
+		checkAnswer
+	}
+}
